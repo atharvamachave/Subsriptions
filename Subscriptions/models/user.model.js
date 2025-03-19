3d6dbc333532ab72c7510f8969be852cdb1f8f25
atharvamachave <machaveatharva@gmail.com>
2025-03-19 10:45:28 +0530

    Add usermodel and subscription model with renewal validator

---
+++ Subscriptions/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'User name is required'],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, 'User email is required'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, 'Please enter a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'User password is required'],
+      minLength: 6,
+      maxLength: 50,
+    },
+  },
+  { Timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
